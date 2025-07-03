45933ed46c82d8c7775610abee3e4604c41e6940
Seungkyu-Han <trust1204@gmail.com>
2025-07-03 21:08:45 +0900

    feat: add message handler

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Get, Post} from '@nestjs/common';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+
+    @Get()
+    listMessages(): string{
+        return "hi Seungkyu!!"
+    }
+
+    @Post()
+    createMessage(@Body() req:any){
+        console.log(req);
+    }
+
+    @Get('/:id')
+    getMessage(){
+
+    }
+}
