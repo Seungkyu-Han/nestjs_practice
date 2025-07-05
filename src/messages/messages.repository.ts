77fcba57bfce03c4cafba9d412abd33317b4676d
Seungkyu-Han <trust1204@gmail.com>
2025-07-05 20:04:41 +0900

    feat: add file repository layer

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -2,14 +2,26 @@ import { readFile, writeFile } from 'fs/promises'
 
 export class MessagesRepository {
     async findOne(id: string){
+        const messages = await readFile(`messages.json`, 'utf8');
+        const messagesJson = JSON.parse(messages);
 
+        return messagesJson[id];
     }
 
     async findAll(){
-
+        const messages = await readFile(`messages.json`, 'utf8');
+        return JSON.parse(messages);
     }
 
-    async create(message: string){
+    async create(content: string){
+        const messages = await readFile(`messages.json`, 'utf8');
 
+        const messagesJson = JSON.parse(messages);
+
+        const id = (Math.random() * 9999)
+
+        messagesJson[id] = {id, content};
+
+        await writeFile(`messages.json`, JSON.stringify(messagesJson), 'utf8');
     }
 }
